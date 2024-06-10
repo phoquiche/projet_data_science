22c4ee8f97e42df1c3f8a9f4ee5f3dbbe5000fda
phoquiche <dustybertin@gmail.com>
2024-06-10 20:04:40 +0200

    entrainement du jeu de données

--- prepare.py
+++ prepare.py
@@ -6,6 +6,8 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 data = pd.read_csv('car_insurance.csv')
 
@@ -58,9 +60,23 @@ numeric_data = data[['age', 'credit_score', 'annual_mileage', 'speeding_violatio
 
 # Calcul des corrélations
 correlation_matrix = numeric_data.corr()
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
 
-# Visualisation de la matrice de corrélation
+# Prédictions sur l'ensemble de test
+y_pred = model.predict(X_test)
+
+# Évaluation des performances du modèle
+accuracy = accuracy_score(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
+class_report = classification_report(y_test, y_pred)
+
+print("Accuracy:", accuracy)
+print("Confusion Matrix:\n", conf_matrix)
+print("Classification Report:\n", class_report)
 plt.figure(figsize=(10, 8))
 sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', vmin=-1, vmax=1)
 plt.title('Matrice de Corrélation')
+sns.pairplot(numeric_data, vars=['age', 'credit_score'], hue='outcome')
+plt.title('Scatter Matrix')
 plt.show()
